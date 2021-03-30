0a8dc0514e0a7c28fd63d64735ac8456bbdd5198
José Pajoc <computacion.programacion.web@gmail.com>
2021-03-30 09:01:31 -0600

    Ventana gráfica incial

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#inicio se puede crear
+from tkinter import *                               #Módulo para entorno gráfico
+
+ventanaInicial = Tk()                               #Objeto de tipo ventana
+ventanaInicial.title('Proyecto 2 - IPC2')
+ventanaInicial.resizable(False, False)              #No permitir cambios al ancho y alto de la ventana
+
+marcoInicial = Frame()
+marcoInicial.pack(fill="both", expand="True")                                 #Marco agregado a la ventana
+marcoInicial.config(width="900", height="650")
+
+ventanaInicial.mainloop()                           #Ejecutar hasta cerrar
