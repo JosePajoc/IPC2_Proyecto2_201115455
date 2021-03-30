6c994a7a37fecafe460b9051f7338145acd3fcf1
José Pajoc <computacion.programacion.web@gmail.com>
2021-03-30 11:41:42 -0600

    Carga del archivo XML con ElementTree

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from tkinter import filedialog                      #Módulo para abrir ventana de selección
+from tkinter import messagebox                      #Módulo para cuadros de mensaje
 from io import open                                 #Módulo para abrir el archivo
 from tkinter import *                               #Módulo para entorno gráfico
 
+import xml.etree.ElementTree as ET                  #importando libreria para manipular XML
+
+#---------------------------------------Manejo del XML-----------------------------------------------------------
+documentoXML = None                                   #Variable para el archivo XML
+matricesRaiz = None                                   #Variable para la etiqueta matrices y así poder iterar
+
+def cargarXML(ruta):
+    global documentoXML
+    global matricesRaiz
+    try:
+        documentoXML = ET.parse(ruta)                           #Conviritendo a legible
+        matricesRaiz = documentoXML.getroot()                   #Obteniendo la raíz
+        messagebox.showinfo("información", "Se cargo con éxtio...\n\nEl archivo se encuentra en:\n" + ruta)
+        procesarXML()
+    except:
+        messagebox.showinfo("información", "No se pudo cargar el archivo...")
+
+def procesarXML():
+    global documentoXML
+    global matricesRaiz
+    
+
+
+
+#----------------------------------------Ventana inicial-----------------------------------------------------------
 ventanaInicial = Tk()                               #Objeto de tipo ventana
 ventanaInicial.title('Proyecto 2 - IPC2')
 ventanaInicial.resizable(False, False)              #No permitir cambios al ancho y alto de la ventana
@@ -9,16 +35,14 @@ ventanaInicial.resizable(False, False)              #No permitir cambios al anch
 marcoInicial = Frame(ventanaInicial, width="900", height="650")
 marcoInicial.pack()                                 #Marco agregado a la ventana
 
-#------------------------------------------------------------------------------------------------------------------
-rutaXML = StringVar()                               #Variable para usar global
-
+#-------------------------------------Método para bótones------------------------------------------------------------
 def buscarXML():
     #Cuadro de dialogo para buscar y luego asignarlo a la variable texto
-    rutaXML.set(filedialog.askopenfilename(title = "Seleccionar archivo XML"))   
+    rutaXML = filedialog.askopenfilename(title = "Seleccionar archivo XML")
+    cargarXML(rutaXML)
+       
     
-#-------------------------------------------------------------------------------------------------------------------
-
-#Widgets
+#------------------------------------------#Widgets------------------------------------------------------------------
 cargarArchivobtn = Button(marcoInicial, text="Cargar archivo XML", command=buscarXML)
 cargarArchivobtn.place(x=50, y=20)
 operacionesbtn = Button(marcoInicial, text="Operaciones")
@@ -27,11 +51,6 @@ reportesArchivobtn = Button(marcoInicial, text="Reportes" )
 reportesArchivobtn.place(x=320, y=20)
 ayudaArchivobtn = Button(marcoInicial, text="Ayuda")
 ayudaArchivobtn.place(x=410, y=20)
-
-rutaTextolbl = Label(marcoInicial, text="El archivo se encuentra en:")
-rutaTextolbl.place(x=50, y=70)
-rutalbl = Label(marcoInicial, textvariable=rutaXML)
-rutalbl.place(x=50, y=90)
 
 
 
