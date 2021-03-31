d07d435fdbfe5802689aeaef2deed931aa5d9642
José Pajoc <computacion.programacion.web@gmail.com>
2021-03-31 11:51:47 -0600

    Mostrar imagenes en el entorno gráfico

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from tkinter import filedialog                      #Módulo para abrir ventana 
 from tkinter import messagebox                      #Módulo para cuadros de mensaje
 from tkinter import *                               #Módulo para entorno gráfico
 from nodoMatrix import listaMatrizOrtogonal         #Módulo para crear lista enlazada simplde de matrices ortogonales
+from PIL import Image, ImageTk                      #Instalar módulo, pip install Pillow, para usar imagenes con más opciones
 
 import xml.etree.ElementTree as ET                  #importando libreria para manipular XML
 
 
-#---------------------------------------Manejo del XML-----------------------------------------------------------
+#-------------------------------------------Ventana inicial-----------------------------------------------------------
+ventanaInicial = Tk()                                           #Objeto de tipo ventana
+ventanaInicial.title('Proyecto 2 - IPC2')
+ventanaInicial.resizable(False, False)                          #No permitir cambios al ancho y alto de la ventana
+
+marcoInicial = Frame(ventanaInicial, width="800", height="550")
+marcoInicial.pack()                                             #Marco agregado a la ventana
+
+
+#--------------------------------------------Manejo del XML------------------------------------------------------------
 documentoXML = None                                             #Variable para el archivo XML
 matricesRaiz = None                                             #Variable para la etiqueta matrices y así poder iterar
 listaMatrix1 = listaMatrizOrtogonal()                 #Creación de lista simple enlazada para las matrices ortogonales
+
 
 def cargarXML(ruta):
     global documentoXML
@@ -23,20 +34,22 @@ def cargarXML(ruta):
     except:
         messagebox.showinfo("información", "No se pudo cargar el archivo...")
 
+
 def procesarXML():
     global documentoXML
     global matricesRaiz
+    
     indice = 0
     for matriz in matricesRaiz:                         #Todas las etiquetas de cada matriz son asignadas al elemento
-        print('El nombre de la matriz es ', matriz[0].text)
+        #print('El nombre de la matriz es ', matriz[0].text)
         filas = int(matriz[1].text)                             #Valor de la etiqueta filas del XML
         columnas = int(matriz[2].text)                          #Valor de la etiqueta columnas del XML
         
         imagenEntrada =  matriz[3].text.replace(' ', '')        #Quitando espacios en blanco de la imagen
         imagenSinSaltos = imagenEntrada.replace('\n','')        #Sustitución de saltos por &
-        print('Filas: ', matriz[1].text)                        #<------------------QUITAR
-        print('Columnas: ', matriz[2].text)                     #<------------------QUITAR
-        print('Imagen: ', imagenSinSaltos)                      #<------------------QUITAR
+        #print('Filas: ', matriz[1].text)                        #<------------------QUITAR
+        #print('Columnas: ', matriz[2].text)                     #<------------------QUITAR
+        #print('Imagen: ', imagenSinSaltos)                      #<------------------QUITAR
         #matrizOrto1 = matrizOrtogonal()                         #Instanciando matriz ortogonal
         #matrizOrto1.llenado(columnas, filas, imagenSinSaltos)   
         #matrizOrto1.mostrarMatriz(columnas, filas)    
@@ -44,20 +57,31 @@ def procesarXML():
         listaMatrix1.insertarFinal(indice, matriz[0].text)      #Se crea el nodo con posición y nombre
         #Se busca el nodo creado, se llama al atributo tipo matriz ortogonal, se llena la matriz con la info de la imagen del XML
         listaMatrix1.buscarPosicionMatriz(indice).matrizOrtogonal.llenado(columnas, filas, imagenSinSaltos)
-        listaMatrix1.buscarPosicionMatriz(indice).matrizOrtogonal.mostrarMatriz(columnas, filas)
+        #listaMatrix1.buscarPosicionMatriz(indice).matrizOrtogonal.mostrarMatriz(columnas, filas)
+        #Crear imagenes a partir de la matriz ortogonal
+        listaMatrix1.buscarPosicionMatriz(indice).matrizOrtogonal.crearGrafo(matriz[0].text, filas, columnas)
         indice = indice + 1 
-        print('----------------------------------------------------------------------------------------------')          
+        #print('----------------------------------------------------------------------------------------------\n')
+    mostrarImagenes()
+        
+    
+def mostrarImagenes():
+    imagen1 = Image.open('grafos/Matriz_1.dot.png')
+    tamanoImagen1 = imagen1.resize((250, 250))
+    renderizadoImagen1 = ImageTk.PhotoImage(tamanoImagen1)
+    imagenlbl = Label(marcoInicial, image=renderizadoImagen1)
+    imagenlbl.image = renderizadoImagen1
+    imagenlbl.place(x=40, y=70)
 
+    imagen2 = Image.open('grafos/Matriz_2.dot.png')
+    tamanoImagen2 = imagen2.resize((250, 250))
+    renderizadoImagen2 = ImageTk.PhotoImage(tamanoImagen2)
+    imagen2lbl = Label(marcoInicial, image=renderizadoImagen2)
+    imagen2lbl.image = renderizadoImagen2
+    imagen2lbl.place(x=300, y=70)
+    
 
-#----------------------------------------Ventana inicial-----------------------------------------------------------
-ventanaInicial = Tk()                                           #Objeto de tipo ventana
-ventanaInicial.title('Proyecto 2 - IPC2')
-ventanaInicial.resizable(False, False)                          #No permitir cambios al ancho y alto de la ventana
-
-marcoInicial = Frame(ventanaInicial, width="800", height="550")
-marcoInicial.pack()                                             #Marco agregado a la ventana
-
-#-------------------------------------Método para bótones------------------------------------------------------------
+#-------------------------------------Abrir cuadro de dialogo para buscar--------------------------------------------
 def buscarXML():
     #Cuadro de dialogo para buscar y luego asignarlo a la variable texto
     rutaXML = filedialog.askopenfilename(title = "Seleccionar archivo XML")
@@ -73,7 +97,6 @@ reportesArchivobtn = Button(marcoInicial, text="Reportes" )
 reportesArchivobtn.place(x=320, y=20)
 ayudaArchivobtn = Button(marcoInicial, text="Ayuda")
 ayudaArchivobtn.place(x=410, y=20)
-
 
 
 ventanaInicial.mainloop()                                       #Ejecutar hasta cerrar
--- matrizOrtogonal.py
+++ matrizOrtogonal.py
@@ -3,6 +3,8 @@ from nodoListaCabeCol import nodoCabeceraColumna
 from nodoListaCabeCol import listaCabeceraVertical
 from nodoListaCabeFila import nodoCabeceraFila
 from nodoListaCabeFila import listaCabeceraHorizontal
+from io import open                          #Importando módulo para crear archivo plano
+from graphviz import render                  #Importando módulo para renderizar desde python, agregar con pip graphviz
 
 class matrizOrtogonal():
     def __init__(self):
@@ -38,10 +40,36 @@ class matrizOrtogonal():
                 temporal2 = temporal2.derecha
             print(salida)
             salida = ''
+    
+    def crearGrafo(self, nombre, filas, columnas):
+        nombreGrafo = 'grafos/' + nombre + '.dot'
+        salidaImagen = open(nombreGrafo, 'w')
+        salidaImagen.write('digraph G { \n')
+        salidaImagen.write('node [shape=plaintext] \n')
+        salidaImagen.write('a [label=<<table border="0" cellborder="1" cellspacing="0"> \n')
+        for i in range(filas):
+            temporal = self.filas.buscarCabeceraHorizontal(i).filaDatos
+            temporal2 = temporal.inicio
+            salidaImagen.write('<tr> \n')
+
+            for j in range(columnas):
+                if temporal2.dato == '-':
+                    salidaImagen.write('\t <td>  </td> \n')
+                else:
+                    salidaImagen.write('\t <td>' + temporal2.dato + '</td> \n') 
+                temporal2 = temporal2.derecha
+            
+            salidaImagen.write('</tr> \n')
+
+        salidaImagen.write('</table>>]; \n')
+        salidaImagen.write('}')
+        salidaImagen.close()
+        render('dot', 'png', nombreGrafo)                 #Renderizar el archivo DOT escrito
 
 '''
 matriz1 = matrizOrtogonal()
 matriz1.llenado(3, 3, 'Esto es u')
 matriz1.mostrarMatriz(3,3)
 #print(matriz1.filas.buscarCabeceraHorizontal(1).filaDatos.inicio.derecha.derecha.arriba.dato)
+matriz1.crearGrafo('matriz de prueba', 3, 3)
 '''
